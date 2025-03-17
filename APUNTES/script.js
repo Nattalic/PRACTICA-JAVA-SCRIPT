3fa3c182c63daa66fcc308ab27c592b2b563045a
Natt - <you@example.com>
2025-03-17 09:54:18 -0500

    ejercicios

---
+++ APUNTES/script.js
@@ -0,0 +1,53 @@
+//Estos mensajes (console) apareceran en la consola//
+
+//Let se usa para variables que cambian//
+let nombre = prompt ('Inrgesa tu Nombre')
+console.log('Hola ' + nombre + '!');
+
+//se usa constante cuando el valor se va a repetir en el codigo)/
+
+//Son los tipos de datos mas basicos que podemos encontrar en JavaScript//
+
+let numero = 10;
+let texto= "Hola Mundo";        //string//
+let verdadero = true;           // boolean
+let falso = false;              //boolean
+let nodefinido;                 //Undefined
+let indefinido =  undefined;    //Undefined
+let nulo = null;                //null
+
+console.log (numero);
+console.log(texto);
+console.log(verdadero);
+console.log(indefinido);
+console.log(null);
+
+//Operadores aritmeticos
+
+//numeros
+
+let a= 20;
+let b= 10;
+
+//propiedad
+
+let suma = a + b;
+let multiplicacion = a * b;
+let division = a / b;
+let resta = a - b;
+let modulo = a % b;
+let exponente = a**b;
+
+let contador = 1;
+contador --;  
+
+//operadores de asignacion
+
+let c= 5;
+let d = c + 5;
+
+c -= 5;
+
+//lo que se muestra en la consola
+
+console.log (c);
